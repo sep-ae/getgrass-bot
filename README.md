336c811c1821c975aa255766b90b5f8dfcf55e16
sepae <septito34@gmail.com>
2024-11-07 06:36:50 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# BOT GRASS AUTO FARMING
+
+
+# Requirements
+
+```
+git clone https://github.com/AirdropFamilyIDN-V2-0/grass.git
+```
+```
+cd grass
+```
+```
+pip install requests
+```
+```
+pip install loguru
+```
+```
+pip install websockets==12.0
+```
+```
+pip install fake_useragent
+```
+```
+pip install websockets_proxy
+```
+```
+python localgrassnode.py
+```
+```
+python localgrasslite.py
+```
+# How to get userid grass
+- login ke web grass , terus inspect / f12 ,  klik console
+- paste
+``` 
+localStorage.getItem('userId')
+```
+- kalau ga bisa paste ketik ```allow pasting``` enter
+- baru paste ```localStorage.getItem('userId')```enter lagi
---
+++ localgrassdesktop.py
@@ -0,0 +1,91 @@
+import asyncio
+import random
+import ssl
+import json
+import time
+import uuid
+from loguru import logger
+from websockets_proxy import Proxy, proxy_connect
+from fake_useragent import UserAgent
+
+user_agent = UserAgent(os='windows', platforms='pc', browsers='chrome')
+random_user_agent = user_agent.random
+
+async def connect_to_wss(socks5_proxy, user_id):
+    device_id = str(uuid.uuid3(uuid.NAMESPACE_DNS, socks5_proxy))
+    logger.info(f"Device ID for user {user_id}: {device_id}")
+    while True:
+        try:
+            await asyncio.sleep(random.randint(1, 10) / 10)
+            custom_headers = {
+                "User-Agent": random_user_agent,
+            }
+            ssl_context = ssl.create_default_context()
+            ssl_context.check_hostname = False
+            ssl_context.verify_mode = ssl.CERT_NONE
+            urilist = ["wss://proxy.wynd.network:4444/", "wss://proxy.wynd.network:4650/"]
+            uri = random.choice(urilist)
+            server_hostname = "proxy.wynd.network"
+            proxy = Proxy.from_url(socks5_proxy)
+            async with proxy_connect(uri, proxy=proxy, ssl=ssl_context, server_hostname=server_hostname,
+                                     extra_headers=custom_headers) as websocket:
+                async def send_ping():
+                    while True:
+                        send_message = json.dumps(
+                            {"id": str(uuid.uuid4()), "version": "1.0.0", "action": "PING", "data": {}})
+                        logger.debug(send_message)
+                        await websocket.send(send_message)
+                        await asyncio.sleep(5)
+
+                await asyncio.sleep(1)
+                asyncio.create_task(send_ping())
+
+                while True:
+                    response = await websocket.recv()
+                    message = json.loads(response)
+                    logger.info(message)
+                    if message.get("action") == "AUTH":
+                        auth_response = {
+                            "id": message["id"],
+                            "origin_action": "AUTH",
+                            "result": {
+                                "browser_id": device_id,
+                                "user_id": user_id,
+                                "user_agent": custom_headers['User-Agent'],
+                                "timestamp": int(time.time()),
+                                "device_type": "desktop",
+                                "version": "4.28.1",
+                            }
+                        }
+                        logger.debug(auth_response)
+                        await websocket.send(json.dumps(auth_response))
+
+                    elif message.get("action") == "PONG":
+                        pong_response = {"id": message["id"], "origin_action": "PONG"}
+                        logger.debug(pong_response)
+                        await websocket.send(json.dumps(pong_response))
+        except Exception as e:
+            logger.error(e)
+            logger.error(socks5_proxy)
+
+
+async def main():
+    with open('user_id.txt', 'r') as user_file:
+        user_ids = user_file.read().splitlines()
+    
+    with open('local_proxies.txt', 'r') as proxy_file:
+        local_proxies = proxy_file.read().splitlines()
+    
+    tasks = []
+    proxies_per_user = 10  # Set jumlah proxy per akun
+    
+    for i, user_id in enumerate(user_ids):
+        # Mengalokasikan 10 proxy untuk setiap akun
+        proxies_for_user = local_proxies[i * proxies_per_user:(i + 1) * proxies_per_user]
+        for proxy in proxies_for_user:
+            tasks.append(asyncio.ensure_future(connect_to_wss(proxy, user_id)))
+    
+    await asyncio.gather(*tasks)
+
+if __name__ == '__main__':
+    asyncio.run(main())
